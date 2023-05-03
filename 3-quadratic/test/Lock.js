2a06ec6e342938477dc5d54e804818ee2bdbadad
Alex Roan <alex.roan@hotmail.com>
2023-05-03 10:26:04 +0100

    Hardhat setup

--- 3-quadratic/test/Lock.js
+++ 3-quadratic/test/Lock.js
@@ -1,7 +1,9 @@
-import { time, loadFixture } from "@nomicfoundation/hardhat-network-helpers";
-import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
-import { expect } from "chai";
-import { ethers } from "hardhat";
+const {
+  time,
+  loadFixture,
+} = require("@nomicfoundation/hardhat-network-helpers");
+const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
+const { expect } = require("chai");
 
 describe("Lock", function () {
   // We define a fixture to reuse the same setup in every test.
