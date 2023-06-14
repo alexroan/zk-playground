b1b34666a9bd25cb5dd6d1d2d1518ad2920fcc50
Alex Roan <alex.roan@hotmail.com>
2023-06-14 15:25:30 +0100

    hardhat-noir

--- 5-quadratic-noir/hardhat.config.ts
+++ 5-quadratic-noir/hardhat.config.ts
@@ -1,8 +1,20 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "hardhat-noir";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.18",
+  solidity: {
+    version: "0.8.18",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  paths: {
+    sources: "./contract",
+  }
 };
 
 export default config;
